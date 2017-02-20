eb0720d71feeaad6ecad8b5ca62e8c2efd413a5d
lllafemme <onnegranditpas@gmail.com>
2017-02-20 18:03:58 +0300

    сделала возможность увеличения при клике

--- javascripts/full.js
+++ javascripts/full.js
@@ -1,24 +1,31 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+//
+//     $('video').click(function () {
+//
+//         var o=$(this).parent();
+//
+//         var src=o.find('video').attr('source src');
+//
+//         var html='<a href="#" class="big"><video  autoplay="autoplay" loop="loop" preload="auto"><source src="a.mp4"></source></video></a>';
+//
+//         o.append(html);
+//
+//         o=o.find('.big');
+//
+//         o.animate({width:'200%',height:'200%'});
+//
+//         o.click(function () {
+//
+//
+//             $(this).remove();
+//         });
+//
+//     });
+//
+// });
 
-    $('a').click(function () {
-
-        var o=$(this).parent();
-
-        var url=o.find('a').attr('url');
-
-        var html='<a href="#" class="clone"><iframe width="560" height="315" src="https://www.youtube.com/embed/13-tta8h-fk" frameborder="0" allowfullscreen></iframe></a>';
-
-        o.append(html);
-
-        o=o.find('.clone');
-
-        o.animate({width:'200%',height:'200%'});
-
-        o.click(function () {
-
-
-            $(this).remove();
-        });
+$(document).ready(function(){
+   $("video").click(function(){    
+        $("video").animate({height: "300px"});
     });
-
 });
