525905d33bc98a1d3d4838ddb9c9098a2fcf83e9
lllafemme <onnegranditpas@gmail.com>
2017-02-26 12:56:28 +0300

    наладила анимацию

--- javascripts/onvideo.js
+++ javascripts/onvideo.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('a.ph.a').on('click', function(ev) {
 
         $(".vA")[0].src += "&autoplay=1";
-        ev.preventDefault();
+        // ev.preventDefault();
 
     });
 });
@@ -11,7 +11,7 @@ $(document).ready(function() {
     $('a.ph.b').on('click', function(ev) {
 
         $(".vB")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
@@ -20,7 +20,7 @@ $(document).ready(function() {
     $('a.ph.c').on('click', function(ev) {
 
         $(".vC")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
@@ -29,7 +29,7 @@ $(document).ready(function() {
     $('a.ph.d').on('click', function(ev) {
 
         $(".vD")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
@@ -38,7 +38,7 @@ $(document).ready(function() {
     $('a.ph.e').on('click', function(ev) {
 
         $(".vE")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
@@ -47,7 +47,7 @@ $(document).ready(function() {
     $('a.ph.f').on('click', function(ev) {
 
         $(".vF")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
@@ -56,63 +56,70 @@ $(document).ready(function() {
     $('a.ph.g').on('click', function(ev) {
 
         $(".vG")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
+
 $(document).ready(function() {
     $('a.ph.h').on('click', function(ev) {
 
         $(".vH")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
+
 $(document).ready(function() {
     $('a.ph.i').on('click', function(ev) {
 
         $(".vI")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
+
 $(document).ready(function() {
     $('a.ph.j').on('click', function(ev) {
 
         $(".vJ")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
+
 $(document).ready(function() {
     $('a.ph.k').on('click', function(ev) {
 
         $(".vK")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
+
 $(document).ready(function() {
     $('a.ph.l').on('click', function(ev) {
 
         $(".vL")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
+
 $(document).ready(function() {
     $('a.ph.m').on('click', function(ev) {
 
         $(".vM")[0].src += "&autoplay=1";
-        ev.preventDefault();
+
 
     });
 });
+
 $(document).ready(function() {
     $('a.ph.n').on('click', function(ev) {
 
         $(".vN")[0].src += "&autoplay=1";
-        ev.preventDefault();
+      
 
     });
 });
