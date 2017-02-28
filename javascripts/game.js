2f2f79e17a7632cf941d8f0908d238d666922fb1
lllafemme <onnegranditpas@gmail.com>
2017-02-28 12:53:47 +0300

    зацкленность видео

--- javascripts/game.js
+++ javascripts/game.js
@@ -25,19 +25,20 @@
 // console.log('yay')
 // }
 
-function getTxt() {
-var film = 'клуб завтрак';
-var inputValue = $('#aud').val();
-
-if (film == inputValue) {
-console.log('урааа')
-} else {
- $('frame').txt('false')
-}
-}
-
-$('#aud').keyup(function(event) {
-if (event.keyCode == 13) {
-getTxt()
-}
-})
+// $(document).keyup(function(event) {
+//   if (event.keyCode == 13) {
+//     getTxt()
+//   }
+// })
+// //
+// function getTxt() {
+// var film = 'клуб завтрак';
+// var inputValue = $('#aud').val();
+//
+//
+// if (film == inputValue) {
+// $('div#frame').txt('nnn')
+// } else {
+// $('div#frame').txt('fff')
+// }
+// }
---
+++ javascripts/none2.js
@@ -0,0 +1,10 @@
+// $(document).keyup(function(event) {
+//   if (event.keyCode == 13) {
+//     none2()
+//   }
+// })
+//
+//
+// function none2() {
+//   $('lightbox').hide('slow')
+// };
